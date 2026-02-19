609fdd375ec3cb8342d3f79fe405a7052d8029a7
RyderMoth <grewem60765@masseyhigh.school.nz>
2026-02-20 10:39:23 +1300

    Simple yes / no checker (incomplete)

--- C_01_yes_no_basic.py
+++ C_01_yes_no_basic.py
@@ -1,10 +1,12 @@
 want_intructions =input("Do you want to see the intructions?") . lower ()
 
-# check the user says yes / no
-if want_intructions == "yes" or want_intructions == "y":
-    print("you said yes")
-elif want_intructions =="no" or want_intructions == "n":
-    print("you said no")
-else:
-    print("please enter yes / no")
+while True:
+    # check the user says yes / no
+    if want_intructions == "yes" or want_intructions == "y":
+        print("you said yes")
+    elif want_intructions =="no" or want_intructions == "n":
+        print("you said no")
+    else:
+        print("please enter yes / no")
 
+print("we done")
